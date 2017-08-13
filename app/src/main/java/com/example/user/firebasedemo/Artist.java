166e1cde5415443c50318f5ff7a89aaee6166a68
diptoo <lm33120@gmail.com>
2017-08-14 01:59:50 +0600

    Initial commit

---
+++ app/src/main/java/com/example/user/firebasedemo/Artist.java
@@ -0,0 +1,33 @@
+package com.example.user.firebasedemo;
+
+/**
+ * Created by User on 8/7/2017.
+ */
+
+public class Artist {
+    String artistId;
+    String artistName;
+    String artistGenre;
+    public Artist()
+    {
+
+    }
+
+    public Artist(String artistId, String artistName, String artistGenre) {
+        this.artistId = artistId;
+        this.artistName = artistName;
+        this.artistGenre = artistGenre;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public String getArtistGenre() {
+        return artistGenre;
+    }
+}
---
+++ app/src/main/java/com/example/user/firebasedemo/Profile_Info2.java
@@ -0,0 +1,13 @@
+package com.example.user.firebasedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Profile_Info2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile__info2);
+    }
+}
